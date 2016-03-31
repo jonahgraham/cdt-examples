d9f6a09c8416bc135f6e4b213ba90d34463e7a36
Jonah Graham <jonah@kichwacoders.com>
2016-04-01 01:45:26 +0200

    Start point for DSF-GDB tutorial

--- org.eclipse.cdt.examples.dsf.gdb/src/org/eclipse/cdt/examples/dsf/gdb/service/command/GdbExtendedCommandFactory_6_8.java
+++ org.eclipse.cdt.examples.dsf.gdb/src/org/eclipse/cdt/examples/dsf/gdb/service/command/GdbExtendedCommandFactory_6_8.java
@@ -12,11 +12,8 @@ package org.eclipse.cdt.examples.dsf.gdb.service.command;
 
 import org.eclipse.cdt.dsf.debug.service.IBreakpoints.IBreakpointsTargetDMContext;
 import org.eclipse.cdt.dsf.debug.service.command.ICommand;
-import org.eclipse.cdt.dsf.debug.service.command.ICommandControlService.ICommandControlDMContext;
 import org.eclipse.cdt.dsf.gdb.service.command.CommandFactory_6_8;
 import org.eclipse.cdt.dsf.mi.service.command.output.MIBreakInsertInfo;
-import org.eclipse.cdt.dsf.mi.service.command.output.MIGDBVersionInfo;
-import org.eclipse.cdt.examples.dsf.gdb.service.command.commands.CLIGDBVersion;
 
 public class GdbExtendedCommandFactory_6_8 extends CommandFactory_6_8 {
 	@Override
@@ -28,9 +25,5 @@ public class GdbExtendedCommandFactory_6_8 extends CommandFactory_6_8 {
 		printfStr = printfStr.replaceFirst("^\"", "\"[EX] "); //$NON-NLS-1$ //$NON-NLS-2$
 		return super.createMIDPrintfInsert(ctx, isTemporary, condition, ignoreCount,
 				tid, disabled, location, printfStr);
-	}
-	
-	public ICommand<MIGDBVersionInfo> createCLIGDBVersion(ICommandControlDMContext ctx) {
-		return new CLIGDBVersion(ctx);
 	}
 }
---
+++ org.eclipse.cdt.examples.dsf.gdb/src/org/eclipse/cdt/examples/dsf/gdb/service/command/commands/package-info.java
@@ -0,0 +1,7 @@
+package org.eclipse.cdt.examples.dsf.gdb.service.command.commands;
+
+/**
+ *
+ * TODO This package would make a good place for the Show Version command
+ * 
+ */
