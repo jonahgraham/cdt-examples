a04867fd0d68c4dd21eec5e37960448df869cd81
Tracy Miranda <tracy@kichwacoders.com>
2016-04-04 20:04:21 +0100

    Add missing copyright

--- com.kichwacoders.cdt.dsf.tutorial/src/com/kichwacoders/cdt/dsf/tutorial/timers/solution/SpyTimersView.java
+++ com.kichwacoders.cdt.dsf.tutorial/src/com/kichwacoders/cdt/dsf/tutorial/timers/solution/SpyTimersView.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Ericsson and others.
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
 package com.kichwacoders.cdt.dsf.tutorial.timers.solution;
 
 import org.eclipse.cdt.dsf.service.DsfServicesTracker;
